52ad318841540aa111313ac45e67e87bd5fdf5cc
RFonzi <ryan.fonzi@gmail.com>
2014-10-26 12:51:49 -0400

    asdf

--- app/src/main/java/acm/ucf/simplereminders/CurrentRemindersActivity.java
+++ app/src/main/java/acm/ucf/simplereminders/CurrentRemindersActivity.java
@@ -42,7 +42,7 @@ public class CurrentRemindersActivity extends Activity {
 
         reminders.add(currentEvent);
 
-        
+        // changeeee
         ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, reminders);
         eventsListView = (ListView) findViewById(R.id.currentRemindersListView);
         eventsListView.setAdapter(arrayAdapter);
